85d8858d04e9b9d4977ebc50a9ac6209ca071171
Bentley Turner <bentley.turner@cognizant.com>
2018-05-29 11:20:02 +1000

    Add View Categories

--- src/actions/types.js
+++ src/actions/types.js
@@ -2,3 +2,5 @@ export const GET_ERRORS = "GET_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
 export const GET_VENDORS = "GET_VENDORS";
 export const VENDOR_LOADING = "VENDOR_LOADING";
+export const GET_CATEGORIES = "GET_CATEGORIES";
+export const CATEGORY_LOADING = "CATEGORY_LOADING";
