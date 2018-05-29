45226b0b0e548b5ab07ea66fd79f38adb9cd77a4
JonathonDonaghy <jonathon.donaghy@cognizant.com>
2018-05-29 12:00:33 +1000

    updated the profile box to better match the PDF

--- src/components/portal/profile/Profile.js
+++ src/components/portal/profile/Profile.js
@@ -6,29 +6,31 @@ class Profile extends Component {
   render() {
     return (
       <div className="profile-sidebar">
-        <div className="profile-userpic">
-          <img src={photoID} className="img-responsive" alt="" />
+        <div className="profile-header">
+          <div className="profile-header-text">Profile</div>
         </div>
+        <div className="smaller-profile-box">
+          <div className="profile-userpic">
+            <img src={photoID} className="img-responsive" alt="" />
+          </div>
 
-        <div className="profile-usertitle">
-          <div className="profile-usertitle-name">Helen Cho</div>
-        </div>
+          <div className="profile-usertitle">
+            <div className="profile-usertitle-name">Helen Cho</div>
+          </div>
 
-        <div className="profile-userbuttons">
-          <button type="button" className="btn btn-outline-info btn-sm">
-            View
-          </button>
-          <button type="button" className="btn btn-outline-info btn-sm">
-            Update
-          </button>
-        </div>
-        <div className="profile-usermenu">
-          <div className="profile-usertitle-name">ID</div>
-          <div className="profile-usertitle-job">GRZLY17234</div>
-          <div className="profile-usertitle-name">Designation</div>
-          <div className="profile-usertitle-job">Sr. Admin</div>
-          <div className="profile-usertitle-name">Office</div>
-          <div className="profile-usertitle-job">NYC, NY, USA</div>
+          <div className="profile-userbuttons">
+            <button type="button" className="btn more-rounded btn-outline-success-secondary btn-sm my-2 my-sm-0 mr-sm-2">
+              View
+            </button>
+          </div>
+          <div className="profile-usermenu">
+            <div className="profile-usertitle-name">ID</div>
+            <div className="profile-usertitle-job">GRZLY17234</div>
+            <div className="profile-usertitle-name">Designation</div>
+            <div className="profile-usertitle-job">Sr. Admin</div>
+            <div className="profile-usertitle-name">Office</div>
+            <div className="profile-usertitle-job">NYC, NY, USA</div>
+          </div>
         </div>
       </div>
     );
