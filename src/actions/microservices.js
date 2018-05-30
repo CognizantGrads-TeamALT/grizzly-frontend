02f5ed6eda3dd56f4c882f71d7377bf066f34ac2
Bentley Turner <bentley.turner@cognizant.com>
2018-05-30 13:59:07 +1000

    Fetching real Categories from Backend

--- src/actions/microservices.js
+++ src/actions/microservices.js
@@ -1,2 +1,4 @@
 export const VENDOR_API_GATEWAY =
   "http://alt.ausgrads.academy:8765/vendormicro/vendor";
+export const CATEGORY_API_GATEWAY =
+  "http://alt.ausgrads.academy:8765/categorymicro/category";
