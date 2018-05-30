41d6af2b74b30df1208849e636ad2b7d42f80146
Steven Phan <thanhviet.phan@cognizant.com>
2018-05-30 14:18:38 +1000

    GRIZ-134 delete vendor

--- src/actions/microservices.js
+++ src/actions/microservices.js
@@ -1,2 +1,2 @@
-export const VENDOR_API_GATEWAY =
-  "http://alt.ausgrads.academy:8765/vendormicro/vendor";
+// export const VENDOR_API_GATEWAY = "http://alt.ausgrads.academy:8765/vendormicro/vendor";
+export const VENDOR_API_GATEWAY = "http://localhost:8765/vendormicro/vendor";
