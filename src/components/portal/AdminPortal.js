8b21c019b3df963d2d8c7ce413b560e2689f8995
Sophia <sophia.huynh@cognizant.com>
2018-06-01 11:35:37 +1000

    Removed the auto margin from the profile box so it doesn't center veritically

--- src/components/portal/AdminPortal.js
+++ src/components/portal/AdminPortal.js
@@ -10,7 +10,7 @@ class AdminPortal extends Component {
   render() {
     return (
       <div className="row">
-        <div className="col-md-3 m-auto">
+        <div className="col-md-3">
           <Profile />
         </div>
         <div className="col-md-9">
