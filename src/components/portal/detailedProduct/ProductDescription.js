5cf6c5fffc682490930642f98da95a06774e2470
jenni <jeniperiasamy@gmail.com>
2018-06-12 14:05:00 +1000

    working on inline edit, trying to handle callback correctly

--- src/components/portal/detailedProduct/ProductDescription.js
+++ src/components/portal/detailedProduct/ProductDescription.js
@@ -1,36 +1,93 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+import EditableLabel from 'react-inline-editing';
+import Button from 'react-ions/lib/components/Button';
+import InlineEdit from 'react-ions/lib/components/InlineEdit';
 
 class ProductDescription extends Component {
     constructor(props) {
         super(props);
         this.state = {
             desc: this.props.productDetails.desc,
-            price: this.props.productDetails.price
+            price: this.props.productDetails.price,
+            isEditing: false,
+            isEditingPrice: false
         }
+        this.handleCallbackDesc = this.handleCallbackDesc.bind(this);
+        this.buttonCallbackDesc = this.buttonCallbackDesc.bind(this);
+        this.handleCallbackPrice = this.handleCallbackPrice.bind(this);
+        this.buttonCallbackPrice = this.buttonCallbackPrice.bind(this);
+        this.onClick = this.onClick.bind(this);
     }
+
+   /*  state = {
+        isEditing: false,
+        //value: 'this.state.price'
+      } */
+    
+      handleCallbackDesc = (event) => {
+        this.setState({ isEditing: false, [event.target.name]: event.target.value })
+      }
+    
+      buttonCallbackDesc = (event) => {
+        this.setState({ isEditing: true, isEditingPrice:false })
+      }
+
+      handleCallbackPrice = (event) => {
+        this.setState({ isEditingPrice: !this.state.isEditingPrice, [event.target.name]: event.target.value })
+      }
+    
+      buttonCallbackPrice = (event) => {
+        this.setState({ isEditingPrice: true, isEditing:false })
+      }
+      onClick =(event)=>
+    {
+        console.log("isEditingPrice" + this.state.isEditingPrice);
+        console.log("isEditingDesc" + this.state.isEditing);
+        console.log(this.props);
+        console.log(this.state);
+    }
+
     render() {
         return (
             <div className="container surround-parent parent-high">
                 <div className="row align-items-start">
                     <div className="col">
                         <div className="mb-3 mt-5">
-                            Product Description
+                            Product Description 
+                            <Button className="d-inline btn far fa-edit d-inline" 
+                            onClick={this.buttonCallbackDesc}
+                             />
                         </div>
                     </div>
                 </div>
                 <div className="row align-items-start parent-min-high">
                     <div className="col-8">
                         <div className="dscrptnSize-7">
-                            <p>{this.state.desc}</p>
+                          {/*}  <p>{this.state.desc}</p> */}
+                          <InlineEdit text={this.state.desc} className="d-inline"
+                            value={this.state.desc} 
+                            isEditing={this.state.isEditing} 
+                            changeCallback={this.handleCallbackDesc} />
+                            
+                          
                         </div>
                     </div>
                     
                 </div>
                 <div className="row align-items-end">
-                    <div className="col">  
-                        <p className="mb-0 bottom-zero bottom-heavy">${this.state.price}</p>
+                    <div className="col d-inline">  
+                       {/*} <p className="mb-0 bottom-zero bottom-heavy d-inline">${this.state.price}</p>*/} 
+                       <InlineEdit className="d-inline" name={this.state.price} 
+                       text={this.state.price} 
+                       placeholder={this.state.price}
+                       isEditing={this.state.isEditingPrice} 
+                       changeCallback={this.handleCallbackPrice} 
+                       
+                       />
+                       <Button className="d-inline btn far fa-edit d-inline" 
+                            onClick={this.buttonCallbackPrice} ></Button>
+                       
                     </div> 
                     <div className="col">  
                         
@@ -38,10 +95,12 @@ class ProductDescription extends Component {
                     <div className="col surround-parent parent-wide">  
                         <div className="row surround-parent parent-wide">
                             <div className="col align-self-end surround-parent parent-wide">
+                            <button onClick={this.onClick} className="btn btn-rounded">test button</button>
                                     <Link 
                                         className="btn more-rounded hover-t-b btn-sm mx-auto surround-parent parent-wide mt-2"
                                         to="/adminportal"
                                         // Please change when you do Edit Product
+
                                             >
                                                 Finish
                                     </Link>
--- src/components/portal/detailedProduct/ProductTitle.js
+++ src/components/portal/detailedProduct/ProductTitle.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import ImageUploader from "../products/ImageUploader";
+import EditableLabel from 'react-inline-editing';
+import InlineEdit from 'react-ions/lib/components/InlineEdit'
+import Button from 'react-ions/lib/components/Button'
+//import styles from './styles'
 
 
 class ProductTitle extends Component {
@@ -8,20 +12,48 @@ class ProductTitle extends Component {
         this.state = {
             detailedProductId: this.props.productId,
             name: this.props.productDetails.name,
-            vendor: this.props.productDetails.vendorId
+            vendor: this.props.productDetails.vendorId,
+           
+            
         }
-        
     }
+
+    state = {
+        isEditing: false,
+        value:'{this.state.name}'
+      }
+    
+      handleCallback = event => {
+        this.setState({ isEditing: false, value: event.target.value })
+        console.log("IN handle callback");
+      }
+    
+      buttonCallback = () => {
+        this.setState({ isEditing: true })
+      }
+    
     render() {
         return (
             <div className="container parent-high">
                 <div className="row align-items-start">
 
                     <div className="col pl-0">
-                        <div className="productTitle">
-                            <b className="d-inline">{this.state.name}</b><p className="d-inline dscrptnSize-9"> by {this.state.vendor}</p>
+                        <div className="productTitle d-inline">
+                           {/* <b className="d-inline">{this.state.name}</b><p className="d-inline dscrptnSize-9"> by {this.state.vendor}</p>
+                            
+                            <EditableLabel text={this.state.name}  className="d-inline" 
+                                onFocus={this._handleFocus} 
+                                onFocusOut={this._handleFocusOut}
+                                onEditDone={this.saveTempName}>                                                       
+                            </EditableLabel>
+                            <button type="button" className="btn far fa-edit d-inline" 
+                            onClick={this.handleClick}></button>*/}
+
+                            <InlineEdit name='test' value={this.state.name} isEditing={this.state.isEditing} 
+                            changeCallback={this.handleCallback} />
+                            <Button className="d-inline btn far fa-edit d-inline" onClick={this.buttonCallback} ></Button>
                         </div>
-                        <div className="productRating">
+                        <div className="productRating d-inline">
                             <i className="d-inline fas fa-star fa-xs mr-1"/><p className="d-inline dscrptnSize-8">4.7</p>
                         </div>
                     </div>
