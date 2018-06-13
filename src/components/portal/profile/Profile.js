aaa9330c19c34ae28dc5ea1129ad466502ee8fab
Sophia <sophia.huynh@cognizant.com>
2018-06-13 14:31:35 +1000

    Profile sidebar now takes data straight dummy db

--- src/components/portal/profile/Profile.js
+++ src/components/portal/profile/Profile.js
@@ -2,18 +2,39 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import photoID from "../../../img/photoID.png";
+import isEmpty from "../../../validation/is-empty";
+import Spinner from "../../common/Spinner";
 import { getUsers } from "../../../actions/userActions";
 
 class Profile extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      userId: 2
+    }
+    this.props.getUsers(this.state.userId);
   }
 
-  getUserDetails(id) {
-    this.props.getUserDetails();
+  show() {
+    const { loading } = this.props;
+    const { user } = this.props.user;
+
+    
+    if (isEmpty(user) || loading) {
+      return (
+
+            <Spinner />
+
+      );
+    } else {
+      return user.filter(
+        user => parseInt(user.userId) === parseInt(this.state.userId, 10)
+      )[0];
+      
+    }
   }
+
   render() {
-    const { userDetails } = this.props;
     return (
       <div className="text-center profile-sidebar">
         <div className="profile-header">
@@ -21,7 +42,6 @@ class Profile extends Component {
              <button type="button" className="btn btn-link hover-def-plain-b profile-header-button">
               Edit
             </button>
-            {this.props}
           </div>
          
         </div>
@@ -31,15 +51,16 @@ class Profile extends Component {
           </div>
 
           <div className="profile-usertitle">
-            <div className="profile-usertitle-name">{this.props.getUserDetails()}</div>
+            <div className="profile-usertitle-name">
+
+            </div>
           </div>
           <div className="profile-usermenu">
-            <div className="profile-usertitle-name">ID</div>
-            <div className="profile-usertitle-job">GRZLY17234</div>
-            <div className="profile-usertitle-name">Designation</div>
-            <div className="profile-usertitle-job">Sr. Admin</div>
-            <div className="profile-usertitle-name">Office</div>
-            <div className="profile-usertitle-job">NYC, NY, USA</div>
+            <div className="profile-usertitle-title fnt-weight-500">{this.show().name}</div>
+            <div className="fnt-weight-400">Contact Number</div>
+            <div className="profile-usertitle-info fnt-weight-300">{this.show().contact_num}</div>
+            <div className="fnt-weight-400">Email</div>
+            <div className="profile-usertitle-info fnt-weight-300">{this.show().email}</div>
           </div>
         </div>
       </div>
@@ -51,5 +72,9 @@ Profile.propTypes = {
   getUsers: PropTypes.func.isRequired
 };
 
+const mapStateToProps = state => ({
+  user: state.user
+});
 
-export default connect(null)(Profile);
+
+export default connect(mapStateToProps, { getUsers })(Profile);
