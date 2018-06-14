990f0f33ebda3954939ecc2a0466b30e40b8764f
Dan Sali <ramadan.sali@cognizant.com>
2018-06-14 14:47:04 +1000

    HTTPS

--- src/actions/microservices.js
+++ src/actions/microservices.js
@@ -1,6 +1,6 @@
 export const VENDOR_API_GATEWAY =
-  'http://api.alt.ausgrads.academy/vendormicro/vendor';
+  'https://api.alt.ausgrads.academy/vendormicro/vendor';
 export const CATEGORY_API_GATEWAY =
-  'http://api.alt.ausgrads.academy/categorymicro/category';
+  'https://api.alt.ausgrads.academy/categorymicro/category';
 export const PRODUCT_API_GATEWAY =
-  'http://api.alt.ausgrads.academy/productmicro/product';
+  'https://api.alt.ausgrads.academy/productmicro/product';
