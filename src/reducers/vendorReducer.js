372db66a1a034f5454be90b8e95e4427158b2195
Steven Phan <thanhviet.phan@cognizant.com>
2018-06-20 10:06:32 +1000

    fix ProductForm, vendor is now not able to search for vendors

--- src/reducers/vendorReducer.js
+++ src/reducers/vendorReducer.js
@@ -1,5 +1,5 @@
-import * as types from "../actions/types";
-import isEmpty from "../validation/is-empty";
+import * as types from '../actions/types';
+import isEmpty from '../validation/is-empty';
 
 const initialState = {
   vendors: null,
@@ -36,7 +36,7 @@ export default function(state = initialState, action) {
         ? currentVendors
         : [
             ...new Map(
-              currentVendors.concat(action.payload).map(o => [o["vendorId"], o])
+              currentVendors.concat(action.payload).map(o => [o['vendorId'], o])
             ).values()
           ];
       return {
@@ -45,6 +45,12 @@ export default function(state = initialState, action) {
         hasMore: hasMore,
         index: index,
         loading: false
+      };
+    case types.VENDOR_TYPEAHEAD_UPDATE:
+      return {
+        ...state,
+        cur_id: action.payload.cur_id,
+        valid_vendor: action.payload.valid_vendor
       };
     case types.VENDOR_ADDING:
       const currentVendors2 = isEmpty(state.vendors) ? [] : state.vendors;
