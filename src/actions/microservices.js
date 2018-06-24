2ac2377cbcae183c24893c8a05505ec0be2b1652
Dan Sali <Ramadan.Sali@Cognizant.com>
2018-06-24 21:42:57 +1000

    Added API gateway url.

--- src/actions/microservices.js
+++ src/actions/microservices.js
@@ -1,8 +1,10 @@
+const AUTH_API_PROD      = 'https://api.alt.ausgrads.academy';
 const CATEGORY_API_PROD  = 'https://api.alt.ausgrads.academy/category';
 const PRODUCT_API_PROD   = 'https://api.alt.ausgrads.academy/product';
 const USER_API_PROD      = 'https://api.alt.ausgrads.academy/user';
 const VENDOR_API_PROD    = 'https://api.alt.ausgrads.academy/vendor';
 
+const AUTH_API_DEV      = 'http://localhost:8765';
 const CATEGORY_API_DEV  = 'http://localhost:10001';
 const PRODUCT_API_DEV   = 'http://localhost:10005';
 const USER_API_DEV      = 'http://localhost:10002';
@@ -13,6 +15,7 @@ let PRODUCTION = false;
 if (process.env.REACT_APP_ENV !== "development")
   PRODUCTION = true;
 
+export const AUTH_API_GATEWAY     = PRODUCTION ? AUTH_API_PROD      : AUTH_API_DEV;
 export const CATEGORY_API_GATEWAY = PRODUCTION ? CATEGORY_API_PROD  : CATEGORY_API_DEV;
 export const PRODUCT_API_GATEWAY  = PRODUCTION ? PRODUCT_API_PROD   : PRODUCT_API_DEV;
 export const USER_API_GATEWAY     = PRODUCTION ? USER_API_PROD      : USER_API_DEV;
