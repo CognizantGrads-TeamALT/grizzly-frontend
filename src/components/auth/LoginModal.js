ac875946e621068f3a4517f93fa23d15283850c7
Steven Phan <thanhviet.phan@cognizant.com>
2018-06-26 09:15:27 +1000

    GRIZ-224 Login functionality

--- src/components/auth/LoginModal.js
+++ src/components/auth/LoginModal.js
@@ -1,23 +1,26 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
-
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { loginUser, logoutUser } from '../../actions/userActions';
+import { GoogleLogin } from 'react-google-login';
+import isEmpty from '../../validation/is-empty';
 
 class LoginModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
       modal: false,
-      username: "",
-      password: ""
+      username: '',
+      password: ''
     };
 
     this.onToggle = this.onToggle.bind(this);
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
   }
 
   onToggle() {
@@ -38,22 +41,31 @@ class LoginModal extends Component {
     this.onToggle();
   }
 
-  // componentDidMount() {
-  // // If already logged, redirect to portal
-  //   if (this.props.auth.isAuthenticated) {
-  //     this.props.history.push("/portal");
-  //   }
-  // }
+  componentDidMount() {
+    // If already logged, redirect to portal
+    if (!isEmpty(this.props.user)) {
+      if (this.props.user.isAuthenticated) {
+        this.props.history.push(`/${this.props.user.userType}`);
+      }
+    }
+  }
 
-  // componentWillReceiveProps(nextProps) {
-  // // If already logged, redirect to portal
-  //   if (nextProps.auth.isAuthenticated) {
-  //     this.props.history.push("/portal");
-  //   }
-  // }
+  componentWillReceiveProps(nextProps) {
+    // If already logged, redirect to portal
+    if (nextProps.user.isAuthenticated) {
+      this.props.history.push(`/${this.props.user.userType}`);
+    }
+  }
 
-  responseGoogle(response) {
-    console.log(response);
+  login(response) {
+    if (isEmpty(response.error) && !isEmpty(response.tokenId)) {
+      this.props.loginUser(response);
+      // this.props.history.push(`/${this.props.user.userType}`);
+    }
+  }
+
+  logout() {
+    this.props.logoutUser();
   }
 
   render() {
@@ -75,13 +87,8 @@ class LoginModal extends Component {
             <GoogleLogin
               clientId="296954481305-plmc2jf1o7j7t0aignvp73arbk2mt3pq.apps.googleusercontent.com"
               buttonText="Login with Google"
-              onSuccess={this.responseGoogle}
-              onFailure={this.responseGoogle}
-              className="btn more-rounded parent-wide min-navbar-button-width hover-w-b btn-sm my-2 my-sm-0"
-            />
-            <GoogleLogout
-              buttonText="Logout"
-              onLogoutSuccess={this.responseGoogle}
+              onSuccess={this.login}
+              onFailure={this.login}
               className="btn more-rounded parent-wide min-navbar-button-width hover-w-b btn-sm my-2 my-sm-0"
             />
           </ModalBody>
@@ -97,11 +104,16 @@ class LoginModal extends Component {
 }
 
 LoginModal.propTypes = {
-  user: PropTypes.object.isRequired
+  user: PropTypes.object.isRequired,
+  loginUser: PropTypes.func.isRequired,
+  logoutUser: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
   user: state.user
 });
 
-export default connect(mapStateToProps)(withRouter(LoginModal));
+export default connect(
+  mapStateToProps,
+  { loginUser, logoutUser }
+)(withRouter(LoginModal));
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,7 +5,7 @@ import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import logo from '../../img/logo.png';
 import LoginModal from '../auth/LoginModal';
-import { clearCurrentUser } from '../../actions/userActions';
+import { logoutUser } from '../../actions/userActions';
 import isEmpty from '../../validation/is-empty';
 import { searchProducts } from '../../actions/productsActions';
 
@@ -34,8 +34,8 @@ class Navbar extends Component {
 
   onLogout(e) {
     e.preventDefault();
-    this.props.clearCurrentUser();
-    this.props.history.push('/');
+    this.props.logoutUser();
+    this.props.history.push('/customer');
   }
 
   logOutBtn() {
@@ -79,6 +79,21 @@ class Navbar extends Component {
           </ul>
         );
       }
+    } else if (!isEmpty(this.props.user.googleProfile)) {
+      return (
+        <ul className="navbar-nav pl-2">
+          <li className="nav-item mr-1 my-auto">
+            <i className="far fa-bell p-t-5 white" />
+          </li>
+          <li className="nav-item mr-1 my-auto">
+            <span>{`Welcome, <${
+              this.props.user.googleProfile.given_name
+            }> `}</span>
+          </li>
+
+          <li className="nav-item">{this.logOutBtn()}</li>
+        </ul>
+      );
     } else
       return (
         <ul className="navbar-nav pl-2">
@@ -154,7 +169,7 @@ class Navbar extends Component {
 
 Navbar.propTypes = {
   searchProducts: PropTypes.func.isRequired,
-  clearCurrentUser: PropTypes.func.isRequired
+  logoutUser: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
@@ -163,5 +178,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { searchProducts, clearCurrentUser }
+  { searchProducts, logoutUser }
 )(withRouter(Navbar));
