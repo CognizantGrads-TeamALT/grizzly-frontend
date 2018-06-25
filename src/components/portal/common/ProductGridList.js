809abf6336ba0d9569fa50fdbe43e1a4725071ff
Sophia <sophia.huynh@cognizant.com>
2018-06-25 16:15:05 +1000

    Cleaned up the borders and alignment of the product grid

--- src/components/portal/common/ProductGridList.js
+++ src/components/portal/common/ProductGridList.js
@@ -13,9 +13,9 @@ class ProductGridList extends Component {
       <img
         key={product.productId}
         src={imgInfo.base64Image}
-        className="img-responsive"
+        className="card-img-top"
         alt=""
-        style={{ width: '150px', height: '150px' }}
+        style={{ width: '150px', height:'150px', objectFit: 'cover' }}
       />
     );
   }
@@ -28,7 +28,7 @@ class ProductGridList extends Component {
         return (
           <img
             src={unavailable}
-            className="img-responsive"
+            className="card-img-top"
             style={{ width: '150px', height: '150px' }}
             alt="Unavailable"
           />
@@ -51,15 +51,21 @@ class ProductGridList extends Component {
         prodArray.push(products[i]);
       }
       return prodArray.map(prod => (
-        <div key={prod.productId} className="col-md-2 col-sm-4 imageGrid mt-5">
-          <Link
-            to={`/customerdetailedproduct/${prod.productId}`}
-            className="img-thumbnail"
-          >
-            {this.showImg(prod)}
+        <div key={prod.productId} className="col-md-2 col-sm-4 imageGrid mt-3">
+            <Link
+              to={`/customerdetailedproduct/${prod.productId}`}
+              className="img-thumbnail surround-parent h-100 w-100 card product-card"
+            >
+              {this.showImg(prod)}
+              <div className="card-body inner-product-card surround-parent h-100 w-100">
+                <div className="inner-product-card card-text fnt-weight-400 surround-parent w-100">{prod.name}</div>
+                {/* Totally mock data */}
+                <div className="fnt-weight-300 dscrptnSize-8 surround-parent w-100">{prod.vendorId}</div>
+                <div className="fnt-weight-300 dscrptnSize-8"><i className="d-inline griz-yellow-color fas fa-star"></i><i className="d-inline griz-yellow-color fas fa-star"></i><i className="d-inline griz-yellow-color fas fa-star"></i><i className="d-inline griz-yellow-color fas fa-star"></i><i className="d-inline griz-yellow-color fas fa-star-half"></i> ({Math.floor((Math.random() * 50) + 1)})</div>
+                <div className="fnt-weight-600 surround-parent w-100">AU${prod.price}.00</div>
+              </div>
+            </Link>
 
-            <span>{prod.name}</span>
-          </Link>
         </div>
       ));
     }
