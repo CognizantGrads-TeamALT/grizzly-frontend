0c9d1e924873a8f6fbc23076e0cd27bdd7d73740
Sophia <sophia.huynh@cognizant.com>
2018-06-26 12:01:52 +1000

    Fixed some styling

--- src/components/portal/customer/UserProfile.js
+++ src/components/portal/customer/UserProfile.js
@@ -7,30 +7,32 @@ import Spinner from '../../common/Spinner';
 import { getUsers } from '../../../actions/userActions';
 
 class UserProfile extends Component {
-// constructor(props) {
-//     super(props);
-// }
-
 render() {
     return (
     <div className="m-3 text-left col">
         <div className="m-2 row">
-            <img src={this.props.user.googleProfile.picture} className="main-profile-img"/>
+            <div className="col-2">
+                <img src={this.props.user.googleProfile.picture} className="main-profile-img"/>
+            </div>
+            <div className="col-10 text-left">
+                <div className="mb-2 row fnt-weight-600 title-size-1-5em">
+                    {this.props.user.googleProfile.given_name} {this.props.user.googleProfile.family_name}
+                </div>
+                <div className="mb-2 row fnt-weight-400 title-size-1em">
+                    {this.props.user.googleProfile.email}
+                </div>
+                <div className="mb-2 row">
+                    <button type="button" className="pl-0 btn btn-link red-alert-minimal-button">Delete Account</button>
+                </div>
+            </div>
         </div>
-        <div className="m-2 row fnt-weight-600 title-size-1-5em">
-            {this.props.user.googleProfile.given_name} {this.props.user.googleProfile.family_name}
-        </div>
-        <div className="m-2 row fnt-weight-400 title-size-1em">
-            {this.props.user.googleProfile.email}
-        </div>
+
+
     </div>
     );
-    }
+}
 }
 
-UserProfile.propTypes = {
-getUsers: PropTypes.func.isRequired
-};
 
 const mapStateToProps = state => ({
 user: state.user
