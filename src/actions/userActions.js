0b06c4eb643097f13f0ed6c41427ac0e49a93dc2
Steven Phan <thanhviet.phan@cognizant.com>
2018-07-09 12:22:36 +1000

    GRIZ-399

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -8,6 +8,7 @@ import {
   stopJWTRefreshChecker
 } from '../utils/RefreshToken';
 import isEmpty from '../validation/is-empty';
+import { emptyCart } from './cartActions';
 
 // Get Admins List
 export const getUsers = (role, id) => dispatch => {
@@ -136,6 +137,7 @@ export const logoutUser = () => dispatch => {
         auth2.disconnect();
       }
     }
+    emptyCart();
   }
 
   axios.put(AUTH_API_GATEWAY + '/logout');
