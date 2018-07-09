928bfa7c5adce7d5c422e47533fec9a25836d17b
Sophia <sophia.huynh@cognizant.com>
2018-07-09 16:17:50 +1000

    Users can now see their details without editing and going to new page

--- src/components/portal/customer/UserProfile.js
+++ src/components/portal/customer/UserProfile.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getUsers } from '../../../actions/userActions';
+import isEmpty from '../../../validation/is-empty';
 
 class UserProfile extends Component {
+  showProfile() {
+    if (!isEmpty(this.props.user.user)) {
+      return(
+        <div className="col-6 text-left">
+          <div className="fnt-weight-500">Contact Number</div>
+          <div className="profile-usertitle-info fnt-weight-300">
+            {this.props.user.user.contact_num}
+          </div>
+          <div className="fnt-weight-500">Email</div>
+          <div className="profile-usertitle-info fnt-weight-300">
+            {this.props.user.googleProfile.email}
+          </div>
+          <div className="fnt-weight-500">Address</div>
+          <div className="profile-usertitle-info wrap-long-name fnt-weight-300">
+            {this.props.user.user.address}
+          </div>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div className="m-3 text-left col">
@@ -14,12 +36,9 @@ class UserProfile extends Component {
               alt="Profile"
             />
           </div>
-          <div className="col-10 text-left">
-            <div className="mb-2 row fnt-weight-600 title-size-1-5em">
+          <div className="col-4 text-left">
+            <div className="mb-2 row fnt-weight-600 wrap-long-name title-size-1-5em">
               {this.props.user.googleProfile.name}
-            </div>
-            <div className="mb-2 row fnt-weight-400 title-size-1em">
-              {this.props.user.googleProfile.email}
             </div>
             <div className="mb-2 row">
               <button
@@ -32,6 +51,7 @@ class UserProfile extends Component {
               </button>
             </div>
           </div>
+          {this.showProfile()}
         </div>
       </div>
     );
